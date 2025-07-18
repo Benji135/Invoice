6eaa88e14727d0333213d24975ca97114321cf11
Benji <acread194@gmail.com>
2025-07-18 18:55:45 +0500

    add name in model

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  fullname: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }, // Store hashed passwords in production!
   email: { type: String, required: true, unique: true },
